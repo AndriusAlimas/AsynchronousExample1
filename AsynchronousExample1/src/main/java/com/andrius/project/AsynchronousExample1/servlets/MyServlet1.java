601219b8b864b55c08c7398583bba662242c62c4
Andrius <andriusjavait@gmail.com>
2017-05-28 15:08:50 +0100

    we created simple html page that just display message after we invoke asynchronous dispatch , but we update our servlet with asynchrounous mode, with small explanation

--- AsynchronousExample1/src/main/java/com/andrius/project/AsynchronousExample1/servlets/MyServlet1.java
+++ AsynchronousExample1/src/main/java/com/andrius/project/AsynchronousExample1/servlets/MyServlet1.java
@@ -1,33 +1,42 @@
 package com.andrius.project.AsynchronousExample1.servlets;
 
 import java.io.IOException;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MyServlet1
- */
+
+// using annotation we created servlet that supports asychronous proccesing: 
 @WebServlet(asyncSupported = true, urlPatterns = { "/MyServlet1" })
 public class MyServlet1 extends HttpServlet {
+	// FIELDS:
 	private static final long serialVersionUID = 1L;
-       
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public MyServlet1() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// we using javax.servlet.ServletRequest startAsync() to get AsyncContext object that
+		// will have original request and response object, unless you use 2 argument method that
+		// you can pass your wrapper servlet request and response object, for now using original no-argument method:
+		final AsyncContext ac = request.startAsync();
+		// now when we have a async context object we can start doing async job mode
+		// with Runnable implementation, there we using anounimous:
+		ac.start(new Runnable() {
+			
+			@Override
+			public void run() {
+				ac.dispatch("/page.html");
+				// ac.complete(); // will cause IllegalStateException, as request already dispatched
+			}
+		});
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
